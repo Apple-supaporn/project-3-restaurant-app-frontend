cce0a05783fc60e18b6290f01df185bcfff807b5
Apple-supaporn <spk.apple@gmail.com>
2023-09-16 16:12:45 -0700

    add bootstrap header and footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import './App.css';
 
+//IMPORT ALL COMPONENTS
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Route, Routes } from 'react-router-dom';
+
+//IMPORT ALL PAGES
+import Home from './pages/Home'
+import Menu from './pages/Menu'
+import About from './pages/About'
+
 function App() {
+
+  //Create a variable called URL
+  const URL = 'http://localhost:3000/'
+
   return (
     <div className="App">
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/Menu' element={<Menu URL={URL}/>}/>
+        <Route exact path='/About' element={<About URL={URL}/>}/>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,23 @@
-const Header = () => {
-    return <h1>Header</h1>
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function ColorSchemesExample() {
+  return (
+    <>
+      <Navbar bg="light" data-bs-theme="dark" className='sticky-top'>
+        <Container>
+          <Navbar.Brand href="home">Name</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="home">Home</Nav.Link>
+            <Nav.Link href="about">About</Nav.Link>
+            <Nav.Link href="menu">Menu</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+    </>
+  );
 }
-export default Header
+
+export default ColorSchemesExample;
