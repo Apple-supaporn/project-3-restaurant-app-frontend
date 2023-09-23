fd9590e1596402845a64ca72bcbdabb6a489ab2d
csfract <Csfractg@gmail.com>
2023-09-22 23:04:52 -0700

    location show, edit, delete

---
+++ src/components/LocationForm.js
@@ -0,0 +1,105 @@
+import {useParams, useNavigate} from 'react-router-dom'
+import {useState} from 'react'
+import {Card, Box, Form, Button} from 'react-bulma-components'
+import { Link } from "react-router-dom";
+
+
+const LocationForm = (props) => {
+    const navigate = useNavigate()
+    const params = useParams() //can use /:id now
+    const location = props.location
+    
+
+    const newForm = {
+        name: "",
+        description: "",
+        address: "",
+        phone: "",
+        hours: "",
+        image:""
+      }
+    
+    const [form, setForm] = useState(location)
+    const { Input, Field, Label } = Form;
+
+    //handleChange function for the form - each keypress is an event we need to update state with
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value })
+    }
+
+    //handle form for updating
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        props.createLocation(form)
+        setForm(newForm)
+        navigate("/location") // optional direct where you want
+    }
+
+    return (
+        <div>
+            <Box className="form-box">
+                <h2 className="is-size-3 has-font-weight-bold">Create New Location </h2>
+                    <form onSubmit={handleSubmit}>
+                        <Field>
+                            <Label>Name</Label>
+                            <Input 
+                                name="name"
+                                value={form?.name}
+                                placeholder='Stromboli'
+                                onChange={(e)=>{handleChange(e)}}/>
+                        </Field>
+                        <Field>
+                            <Label>Description</Label>
+                            <Input 
+                                name="description"
+                                value={form?.description}
+                                placeholder='We opened our first restaurant in the summer of 2004 in Palo Alto, California!
+                                '
+                                onChange={(e)=>{handleChange(e)}}/>
+                        </Field>
+                        <Field>
+                            <Label>Address:</Label>
+                            <Input 
+                                name="address"
+                                value={form?.address}
+                                placeholder='1875 S. Bascom Ave. Campbell, CA 95008'
+                                onChange={(e)=>{handleChange(e)}}/>
+                        </Field>
+                        <Field>
+                            <Label>Phone:</Label>
+                            <Input 
+                                name="phone"
+                                value={form?.phone}
+                                placeholder='999-999-9999'
+                                onChange={(e)=>{handleChange(e)}}/>
+                        </Field>
+                        <Field>
+                            <Label>Hours:</Label>
+                            <Input 
+                                name="hours"
+                                value={form?.hours}
+                                placeholder='Sunday–Thursday: 11 am to 9 pm Friday–Saturday: 11 am to 10 pm'
+                                onChange={(e)=>{handleChange(e)}}/>
+                        </Field>
+                        
+                        <Field>
+                            <Label>Image:</Label>
+                            <Input 
+                                name="image"
+                                value={form?.image}
+                                placeholder='image.jpg'
+                                onChange={(e)=>{handleChange(e)}}/>
+                        </Field>
+
+                        <div className="buttons is-left">
+                            <Button className="is-primary">Submit</Button> 
+                            <Link to="/location"> <Button className="is-light">Cancel</Button> </Link>
+                        </div>
+
+                    </form>
+            </Box>
+        </div>
+    )
+}
+
+export default LocationForm
